bdeee35a6756565d44a149059e49f445063d2e9e
acroisie <acroisie@student.42lyon.fr>
2023-01-04 16:14:14 +0100

    WIP

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -1,7 +1,5 @@
 #pragma		once
-
-#include	<iostream>
-// #include	<memory>
+#include	<memory>
 
 namespace ft
 {
@@ -28,40 +26,67 @@ namespace ft
 		size_type		_capacity;
 		allocator_type	_alloc;
 
-	public:
 //Member functions------------------------------------------------------------//
+		void	realloc (size_type newCapacity)
+		{
+			T* newData = _alloc.allocate(newCapacity);
+			// _alloc.construct(newData, ); 
+			if (newCapacity < _size)
+				_size = newCapacity;
+			for (size_type i = 0; i < _size; i++)
+				newData[i] = _data[i];
+			_alloc.deallocate(_data, _capacity);
+			_data = newData;
+			_capacity = newCapacity;
+		}
+
+	public:
 	//Constructor/Destructor--------------------------------------------------//
-		vector(void): _data(NULL), _size(0), _capacity(0){resize(2);};
+		vector(void): _data(NULL), _size(0), _capacity(0){realloc(2);};
 		~vector(){};
+		vector& operator= (const vector& x)
+		{
+			_data = x._data;
+			_size = x._size;
+			_capacity = x._capacity;
+			return (*this);
+		}
 
 	//Iterators---------------------------------------------------------------//
 
 	//Capacity----------------------------------------------------------------//
 		size_type			size() const {return(_size);}
 		size_type			max_size() const {return(_alloc.max_size());}
-		void				resize (size_type n, value_type val = value_type())
+		void				resize(size_type n, value_type val = value_type()); //To define
+		size_type			capacity() const {return(_capacity);}
+		bool				empty() const
 		{
-			(void)val; //For the moment
-			T* newData = _alloc.allocate(n);
-			if (n < _size)
-				_size = n;
-			for (size_type i = 0; i < _size; i++)
-				newData[i] = _data[i];
-
-			_alloc.deallocate(_data, _capacity);
-			_data = newData;
-			_capacity = n;
+			if (_size)
+				return (false);
+			return (true);
+		}
+		void 				reserve (size_type n)
+		{
+			if (n > _size)
+				realloc(n);
+		}
+		void				shrink_to_fit()
+		{
+			if (_capacity > _size)
+				realloc(_size);
 		}
 
 	//Element access----------------------------------------------------------//
-		const value_type&	operator[](size_type index) const {return(_data[index]);}
-		value_type&			operator[](size_type index) {return(_data[index]);}
+		reference			operator[](size_type n) {return(_data[n]);}
+		const_reference		operator[](size_type n) const {return(_data[n]);}
+		reference			at(size_type n) {return(_data[n]);}
+		const_reference		at(size_type n) const {return(_data[n]);}
 
 	//Modifiers---------------------------------------------------------------//
-		void				push_back(const value_type& value)
+		void				push_back(const_reference value)
 		{
 			if (_size == _capacity)
-				vector::resize(_capacity * 2);
+				vector::realloc(_capacity * 2);
 			_data[_size++] = value;
 		}
 
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,9 +1,11 @@
 #include	"../includes/vector.hpp"
+#include	<iostream>
 #include	<vector>
 
 template <class T>
 void	printVector(const ft::vector<T>& vector)
 {
+	std::cout << "//New Vector-----//" << std::endl;
 	for (size_t i = 0; i < vector.size(); i++)
 		std::cout << vector[i] << std::endl;
 }
@@ -11,12 +13,27 @@ void	printVector(const ft::vector<T>& vector)
 int	main(void)
 {
 	ft::vector<std::string> strVector;
+	ft::vector<std::string> strVector2;
+	ft::vector<std::string> emptyVector;
 
 	strVector.push_back("1rst");
 	strVector.push_back("2nd");
 	strVector.push_back("3rd");
 	strVector.push_back("4rth");
+	strVector.push_back("5th");
 	printVector(strVector);
+	std::cout << "Capacity ; " << strVector.capacity() << std::endl;
+	strVector.shrink_to_fit();
+	std::cout << "Capacity ; " << strVector.capacity() << std::endl;
+
+	strVector2 = strVector;
+	printVector(strVector2);
+	std::cout << "Capacity ; " << strVector.capacity() << std::endl;
+
+	if (!strVector2.empty())
+		std::cout << "Not empty" << std::endl;
+	if (emptyVector.empty())
+		std::cout << "Empty" << std::endl;
 
 	return (0);
 }
