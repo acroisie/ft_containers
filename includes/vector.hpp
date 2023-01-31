c9d60bf67410310a2d495d791da1ae10224f3744
acroisie <acroisie@student.42lyon.fr>
2023-01-31 16:55:24 +0100

    minor up

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -8,18 +8,37 @@ namespace ft
 	class vector
 	{
 	public:
-		typedef T                                        value_type;
-		typedef Allocator                                allocator_type;
-		typedef typename allocator_type::reference       reference;
-		typedef typename allocator_type::const_reference const_reference;
-		typedef typename allocator_type::pointer         pointer;
-		typedef typename allocator_type::const_pointer   const_pointer;
-		// typedef std::reverse_iterator<iterator>          reverse_iterator;
-		// typedef iterator              				     iterator;
-		// typedef const_iterator         			         const_iterator;
-		// typedef std::reverse_iterator<const_iterator>    const_reverse_iterator;
-		typedef typename allocator_type::difference_type difference_type;
-		typedef typename allocator_type::size_type       size_type;
+		typedef T												value_type;
+		typedef Allocator										allocator_type;
+		typedef typename allocator_type::reference				reference;
+		typedef typename allocator_type::const_reference		const_reference;
+		typedef typename allocator_type::pointer				pointer;
+		typedef typename allocator_type::const_pointer			const_pointer;
+		typedef typename ft::iterator							iterator;
+		typedef typename ft::const_iterator						const_iterator;
+		// typedef typename ft::reverse_iterator<iterator>		reverse_iterator;
+		// typedef typename ft::reverse_iterator<const_iterator>	const_reverse_iterator;
+		typedef typename allocator_type::difference_type		difference_type;
+		typedef typename allocator_type::size_type				size_type;
+
+//Iterator--------------------------------------------------------------------//
+		class	iterator
+		{
+			private:
+				pointer	_ptr;
+
+			public:
+				iterator() : _ptr(nullptr) {}
+				iterator(pointer ptr) : _ptr(ptr) {}
+				~iterator() {}
+
+				typedef value_type = T;
+				typedef reference = T&;
+				typedef pointer = T*;
+				typedef difference_type = ptrdiff_t;
+				// typedef iterator_category = std::random_access_iterator_tag;
+
+		};
 
 	private:
 		value_type*		_data;
@@ -28,7 +47,7 @@ namespace ft
 		allocator_type	_alloc;
 
 //Member functions------------------------------------------------------------//
-		void			realloc (size_type newCapacity)
+		void					realloc (size_type newCapacity)
 		{
 			T* newData = _alloc.allocate(newCapacity);
 			// _alloc.construct(newData, );
@@ -43,9 +62,9 @@ namespace ft
 
 	public:
 	//Constructor/Destructor--------------------------------------------------//
-		vector(void): _data(NULL), _size(0), _capacity(0){realloc(2);};
-		~vector(){};
-		vector&				operator= (const vector& x) // To modify with deep copy i think
+		vector(void): _data(NULL), _size(0), _capacity(0){realloc(2);}
+		~vector(){}
+		vector&					operator= (const vector& x) // To modify with deep copy i think
 		{
 			_data = x._data;
 			_size = x._size;
@@ -54,61 +73,61 @@ namespace ft
 		}
 
 	//Iterators---------------------------------------------------------------//
-		iterator begin(){return (iterator(_data));};
-		const_iterator begin() const {return const_iterator((_data));};
-		iterator end(){return (iterator(_data + _size));};
-		const_iterator end() const {return const_iterator((_data + _size));};
-		reverse_iterator rbegin() {return (this->end);};
-		const_reverse_iterator rbegin() const {return (this->end);};
-		reverse_iterator rend(){return (reverse_iterator(_data));};
-		const_reverse_iterator rend() const {return const_reverse_iterator((_data));};
+		iterator				begin(){return (iterator(_data));}
+		const_iterator			begin() const {return const_iterator((_data));}
+		iterator				end(){return (iterator(_data + _size));}
+		const_iterator			end() const {return const_iterator((_data + _size));}
+		reverse_iterator		rbegin() {return (this->end);}
+		const_reverse_iterator	rbegin() const {return (this->end);}
+		reverse_iterator 		rend(){return (reverse_iterator(_data));}
+		const_reverse_iterator	rend() const {return const_reverse_iterator((_data));}
 
 	//Capacity----------------------------------------------------------------//
-		size_type			size() const {return(_size);}
-		size_type			max_size() const {return(_alloc.max_size());}
-		void				resize(size_type n, value_type val = value_type()); //To define
-		size_type			capacity() const {return(_capacity);}
-		bool				empty() const
+		size_type				size() const {return(_size);}
+		size_type				max_size() const {return(_alloc.max_size());}
+		void					resize(size_type n, value_type val = value_type()); //To define
+		size_type				capacity() const {return(_capacity);}
+		bool					empty() const
 		{
 			if (_size)
 				return (false);
 			return (true);
 		}
-		void 				reserve (size_type n)
+		void 					reserve (size_type n)
 		{
 			if (n > _size)
 				realloc(n);
 		}
-		void				shrink_to_fit()
+		void					shrink_to_fit()
 		{
 			if (_capacity > _size)
 				realloc(_size);
 		}
 
 	//Element access----------------------------------------------------------//
-		reference			operator[](size_type n) {return(_data[n]);}
-		const_reference		operator[](size_type n) const {return(_data[n]);}
-		reference			at(size_type n)
+		reference				operator[](size_type n) {return(_data[n]);}
+		const_reference			operator[](size_type n) const {return(_data[n]);}
+		reference				at(size_type n)
 		{
 			if (n >= _size)
 				throw std::out_of_range("vector");
 			return(_data[n]);
 		}
-		const_reference		at(size_type n) const 
+		const_reference			at(size_type n) const 
 		{
 			if (n >= _size)
 				throw std::out_of_range("vector");
 			return(_data[n]);
 		}
-		reference 			back() {return(_data[_size - 1]);}
-		const_reference		back() const {return(_data[_size - 1]);}
-		reference 			front() {return(_data[0]);}
-		const_reference		front() const {return(_data[0]);}
-		value_type* 		data() {return(&_data[0]);}
-		const value_type*	data() const {return(&_data[0]);}
+		reference 				back() {return(_data[_size - 1]);}
+		const_reference			back() const {return(_data[_size - 1]);}
+		reference 				front() {return(_data[0]);}
+		const_reference			front() const {return(_data[0]);}
+		value_type* 			data() {return(&_data[0]);}
+		const value_type*		data() const {return(&_data[0]);}
 
 	//Modifiers---------------------------------------------------------------//
-		void				push_back(const_reference value)
+		void					push_back(const_reference value)
 		{
 			if (_size == _capacity)
 				vector::realloc(_capacity * 2);
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -46,7 +46,5 @@ int	main(void)
 	for (unsigned i=0; i<strVector2.size(); ++i)
 		std::cout << ' ' << strVector2[i] << std::endl;
 
-
-
 	return (0);
 }
