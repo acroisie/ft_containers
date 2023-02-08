8c5d71409e55377e9feb0bf2751326e735278746
acroisie <acroisie@student.42lyon.fr>
2023-02-08 10:51:27 +0100

    WIP on ra_iterator

--- includes/random_access_iterator.hpp
+++ includes/random_access_iterator.hpp
@@ -5,19 +5,39 @@
 namespace	ft
 {
 	template<class T>
-	class	random_access_iterator: public ft::iterator<std::random_access_iterator_tag, T>
+	class	ra_iterator:
+	public ft::iterator<std::random_access_iterator_tag, T>
 	{
-		public:
-			// typedef typename ft::iterator<std::random_access_iterator_tag, T>::difference_type		difference_type;
-			// typedef typename ft::iterator<std::random_access_iterator_tag, T>::value_type			value_type;
-			typedef typename ft::iterator<std::random_access_iterator_tag, T>::pointer				pointer;
-			// typedef typename ft::iterator<std::random_access_iterator_tag, T>::reference			reference;
-			// typedef typename ft::iterator<std::random_access_iterator_tag, T>::iterator_category	iterator_category;
-		private:
-			pointer	_ptr;
-		public:
-			random_access_iterator(void): _ptr(0) {}
-			random_access_iterator(pointer ptr): _ptr(ptr) {}
-			~random_access_iterator() {}
+	public:
+		typedef typename ft::iterator<std::random_access_iterator_tag, T>
+		::difference_type	difference_type;
+		typedef typename ft::iterator<std::random_access_iterator_tag, T>
+		::value_type		value_type;
+		typedef typename ft::iterator<std::random_access_iterator_tag, T>
+		::pointer			pointer;
+		typedef typename ft::iterator<std::random_access_iterator_tag, T>
+		::reference			reference;
+		typedef typename ft::iterator<std::random_access_iterator_tag, T>
+		::iterator_category	iterator_category;
+
+	private:
+		pointer	_ptr;
+
+	public:
+//Property -------------------------------------------------------------------//
+	//Constructor/Destructor -------------------------------------------------//
+		ra_iterator(): _ptr(NULL) {}
+		ra_iterator(pointer ptr): _ptr(ptr) {}
+		ra_iterator(const ra_iterator& cpy) {*this = cpy;}
+		~ra_iterator() {}
+		ra_iterator&	operator=(const ra_iterator obj)
+		{
+			_ptr = obj._ptr;
+			return (*this);
+		}
+
+	//Equality/Inequality ----------------------------------------------------//
+	bool	operator==(ra_iterator const &obj) const {return (_ptr == obj._ptr);}
+	bool	operator!=(ra_iterator const &obj) const {return (_ptr != obj._ptr);}
 	};
 }
