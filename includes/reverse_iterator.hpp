5e59049c07c221e75c35b945aacb601fa1b86aeb
Arnaud <acroisie@student.42lyon.fr>
2023-02-22 09:41:41 +0100

    Modify reverse_it

--- includes/reverse_iterator.hpp
+++ includes/reverse_iterator.hpp
@@ -9,51 +9,61 @@ namespace		ft
 	class		reverse_iterator
 	{
 	public:
-		typedef Iter			 										iterator_type;
-		typedef typename ft::iterator_traits<Iter>::difference_type		difference_type;
-		typedef typename ft::iterator_traits<Iter>::value_type			value_type;
-		typedef typename ft::iterator_traits<Iter>::pointer				pointer;
-		typedef typename ft::iterator_traits<Iter>::reference			reference;
-		typedef typename ft::iterator_traits<Iter>::iterator_category	iterator_category;
+		typedef				Iter iterator_type;
+		typedef typename	iterator_traits<Iter>::difference_type difference_type;
+		typedef typename	iterator_traits<Iter>::value_type value_type;
+		typedef typename	iterator_traits<Iter>::pointer pointer;
+		typedef typename	iterator_traits<Iter>::reference reference;
+		typedef typename	iterator_traits<Iter>::iterator_category iterator_category;
 
 	protected:
-		iterator_type	_it;
+		Iter _it;
 
 	public:
 //Member functions -----------------------------------------------------------//
-	//Constructor/Destructor/Assign content ----------------------------------//
+	//Constructor/Destructor/Assign operator ---------------------------------//
 		reverse_iterator(): _it() {}
-		explicit reverse_iterator(iterator_type it):_it(it) {}
-		template <class iterator_type>
-		reverse_iterator(const reverse_iterator<iterator_type>& rev_it)
+		explicit reverse_iterator(Iter it): _it(it) {}
+		template <class Iter2>
+		reverse_iterator(const reverse_iterator<Iter2> &rev_it)
 		: _it(rev_it.base()) {}
 
 		~reverse_iterator() {}
 
-		template <class U>
-		reverse_iterator&	operator=(const reverse_iterator<U>& other)
+		reverse_iterator &operator=(const reverse_iterator<Iter> &other)
 		{
 			_it = other._it;
 			return (*this);
 		}
 
-	//Access -----------------------------------------------------------------//
-		iterator_type		base() const {return (_it);}
+	//Member access operator ------------------------------------------------//
+		Iter base() const {return (_it);}
 
-	//Dereference ------------------------------------------------------------//
-		reference 			operator*() const {Iter tmp = _it; return (*--tmp);}
-		pointer 			operator->() const {return &(operator*());}
-		reference 			operator[](difference_type n) const {return (base()[-n - 1]);}
+	//Dereference operators -------------------------------------------------//
+		reference			operator*() const
+		{
+			Iter tmp = _it;
+			return (*--tmp);
+		}
+		pointer				operator->() const {return &(operator*());}
+		reference			operator[](difference_type n) const
+		{
+			return (base()[-n - 1]);
+		}
 
-	//Increment --------------------------------------------------------------//
-		reverse_iterator&	operator++() {return (_it--);}
+	//Increment/decrement operators -----------------------------------------//
+		reverse_iterator	&operator++()
+		{
+			_it--;
+			return (*this);
+		}
 		reverse_iterator	operator++(int)
 		{
-			reverse_iterator cpy(*this);
+			reverse_iterator<Iter> tmp(*this);
 			_it--;
-			return (cpy);
+			return (tmp);
 		}
-		reverse_iterator&	operator+=(difference_type n)
+		reverse_iterator	&operator+=(difference_type n)
 		{
 			_it -= n;
 			return (*this);
@@ -62,27 +72,28 @@ namespace		ft
 		{
 			return reverse_iterator(_it - n);
 		}
-
-
-	//Decrement --------------------------------------------------------------//
-		reverse_iterator&	operator--() {return (_it++);}
+		reverse_iterator	&operator--()
+		{
+			_it++;
+			return (*this);
+		}
 		reverse_iterator	operator--(int)
 		{
-			reverse_iterator cpy(*this);
+			reverse_iterator<Iter> tmp(*this);
 			_it++;
-			return (cpy);
+			return (tmp);
 		}
-		reverse_iterator&	operator-=(difference_type n)
+		reverse_iterator	&operator-=(difference_type n)
 		{
-			_it -= n;
+			_it += n;
 			return (*this);
 		}
 		reverse_iterator	operator-(difference_type n) const
 		{
 			return reverse_iterator(_it + n);
 		}
-
 	};
+
 }
 
 //Non-ember functions --------------------------------------------------------//
