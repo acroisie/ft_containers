64d080ab8992ae821d9bd66e225ca9f58b48d725
Arnaud <acroisie@student.42lyon.fr>
2023-03-07 08:30:24 +0100

    delete useless getters

--- includes/node.hpp
+++ includes/node.hpp
@@ -83,9 +83,6 @@ namespace	ft
         int					getHeight() const {return (_height);}
         void				setHeight(int height) {_height = height;}
 
-        void                first() const {return (_pair.first);}
-        void                second() const {return (_pair.second);}
-
     };
 
 }
